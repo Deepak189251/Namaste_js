e9bbd871112c09183f9c0496a92f124fcf3e7f84
D <deepakkumarbehera2002@gmail.com>
2023-05-18 19:36:50 +0530

    added map-filter-reduce file

--- functions.js
+++ functions.js
@@ -52,7 +52,7 @@ const num = 10;
 
 // Playing with setTimeOut in Local Environment.
 
-
+/*
 console.log(`start`)
 
 setTimeout(function cb(){
@@ -70,3 +70,79 @@ while (endDate < startDate + 10000){
 }
 
 console.log(`Loop completed`);
+
+
+
+    for(var i = 1; i <= 5; i++){
+    function timeout (num){
+        setTimeout(function st(){
+            console.log(num)
+         }, num * 1000)
+    }
+   timeout (i);
+  }
+
+
+
+for (var i = 1; i <= 5; i++){
+    setTimeout(function cb(){
+        console.log(i)
+    }, i * 1000)
+}
+*/
+
+const users = [
+    {firstname: "Deepak", lastname: "Behera", age: 26},
+    {firstname: "Smruti", lastname: "mallick", age: 75},
+    {firstname: "Duryodhan", lastname: "Behera", age: 50},
+    {firstname: "Kartik", lastname: "Behera", age: 26}
+];
+
+
+// This is use of map method : It will return an array which contains full name of each object
+
+const output = users.map((user)=>{
+    return user.firstname + " "+ user.lastname 
+})
+
+
+// This is use of filter method : It will return an array which contains objects who satisfy the given condition.
+
+const output2 = users.filter((user) => {
+    return user.lastname === "Behera"
+})
+
+// This is use of reduce method
+
+const output3 = users.reduce((acc, curr)=>{
+   if(acc[curr.age]){
+    acc[curr.age] = ++acc[curr.age]
+   }
+   else{
+    acc[curr.age] = 1;
+   }
+   return acc
+},{})
+
+
+// This is called chaining filter and map, here first we use filter to filter objects which age is less than 30 then we use map on it to take the first name out of it.
+
+const output4 = users.filter((x) => {
+    return x.age < 30 
+}).map((user) => {
+    return user.firstname
+})
+
+console.log(output)
+
+console.log(output2)
+
+
+const output5 = users.reduce((acc, curr) => {
+   if(curr.age < 30){
+    acc.push(curr.firstname);
+   }
+   return acc
+}, []) 
+
+console.log(output5)
